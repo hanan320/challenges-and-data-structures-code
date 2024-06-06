43b7b290c83648ec914b0c300c532ce1fd144616
HANAN ALHADE <23041786@student.ltuc.com>
2024-06-06 23:46:25 +0300

    Add project files.

---
+++ challenges-and-data-structures-code/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace challenges_and_data_structures_code
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
