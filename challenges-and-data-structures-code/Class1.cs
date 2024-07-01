f2f54e5a7253dae411a7b3e7cdcd83fac4391214
HANAN ALHADE <23041786@student.ltuc.com>
2024-07-01 22:37:21 +0300

    cc6

---
+++ challenges-and-data-structures-code/Class1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace challenges_and_data_structures_code
+{
+    public class Class1
+    {
+        public static int[] CommonElements(int[] array1, int[] array2)
+        {
+            int max1 = FindMaxValue(array1);
+            int max2 = FindMaxValue(array2);
+            int maxPossible = Math.Max(max1, max2);
+
+            int[] freq1 = new int[maxPossible + 1];
+            int[] freq2 = new int[maxPossible + 1];
+
+            PopulateFrequency(freq1, array1);
+            PopulateFrequency(freq2, array2);
+
+            List<int> commonElements = new List<int>();
+            for (int i = 0; i <= maxPossible; i++)
+            {
+                if (freq1[i] > 0 && freq2[i] > 0)
+                {
+                    commonElements.Add(i);
+                }
+            }
+
+            return commonElements.ToArray();
+        }
+        public static int FindMaxValue(int[] arr)
+        {
+            if (arr == null || arr.Length == 0)
+            {
+                throw new ArgumentException("Array must not be empty or null.");
+            }
+
+            int max = int.MinValue;
+            foreach (int num in arr)
+            {
+                if (num > max)
+                {
+                    max = num;
+                }
+            }
+            return max;
+        }
+        private static void PopulateFrequency(int[] freq, int[] arr)
+        {
+            foreach (int num in arr)
+            {
+                freq[num]++;
+            }
+        }
+    }
+}
