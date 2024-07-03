efb1a8887372650f091b7f4d8fcc65c58265d650
HANAN ALHADE <23041786@student.ltuc.com>
2024-07-03 23:41:28 +0300

    Separate file Find Duplicates

---
+++ challenges-and-data-structures-code/Find Duplicates/Find Duplicates/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Find_Duplicates
+{
+    public class Program
+    {
+        public static void findDuplicates(int[] arr, int max)
+        {
+            int[] frequency = new int[max + 1];
+
+            foreach (int i in arr)
+            {
+                frequency[i]++;
+            }
+
+            List<int> duplicates = new List<int>();
+            for (int i = 0; i < frequency.Length; i++)
+            {
+                if (frequency[i] > 1)
+                {
+                    duplicates.Add(i);
+                }
+            }
+            Console.Write("duplicates element: ");
+            Console.WriteLine(string.Join(", ", duplicates));
+        }
+        static void Main(string[] args)
+        {
+            int[] arr1 = { 1, 2, 3, 1, 2, 3 };
+
+            int maxValue = arr1.Max();
+
+            findDuplicates(arr1, maxValue);
+            
+        }
+    }
+}
+        
